9629b25e1fe8c0c965ff8d7f387f945fb1b4a571
Kevin Musselman <kmussel@gmail.com>
2020-07-25 14:23:04 -0400

    add dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
-#!/bin/sh -l
+#!/bin/sh
 
-git clone --quiet https://github.com/$REPO &> /dev/null
+if [ -n "$GITHUB_WORKSPACE" ]; then
+  cd "$GITHUB_WORKSPACE" || exit
+fi
+
+if [ -n "$RUN_DIRECTORY" ]; then
+  cd "$RUN_DIRECTORY"
+fi
+
+if [ -z "$INPUT_REPO" ]; then INPUT_REPO="$GITHUB_REPOSITORY"; fi
+
+
+# if [ -n "$INPUT_TAG" ]; then ARG_TAG="-t $INPUT_TAG"; fi
+
+# git clone --quiet https://github.com/$REPO &> /dev/null
+echo "generate-changelog $* --file -"
 
 changelog=$(generate-changelog $* --file -)
 
